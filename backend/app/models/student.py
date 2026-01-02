b0ad7ad9a696fab3cb843f34907cbc762880539d
tungtranquang271 <126157335+tungtranquang271@users.noreply.github.com>
2026-01-02 20:44:28 +0700

    fix: change date to datetime in student model

--- backend/app/models/student.py
+++ backend/app/models/student.py
@@ -1,13 +1,13 @@
 from pydantic import BaseModel
 from typing import Optional
-from datetime import date
+from datetime import datetime
 
 class StudentBase(BaseModel):
     student_id: str
     first_name: Optional[str] = None
     last_name: Optional[str] = None
     email: Optional[str] = None
-    date_of_birth: Optional[date] = None
+    date_of_birth: Optional[datetime] = None
     hometown: Optional[str] = None
     math: Optional[float] = None
     literature: Optional[float] = None
